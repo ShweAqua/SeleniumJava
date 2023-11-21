78b098039df16ddb3d6218d8e06eeee6c474feb9
AquaShwe <warhadkar002sh@gmail.com>
2023-11-21 22:47:06 +0530

    First Commit

---
+++ src/CoreJavaConcepts.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class CoreJavaConcepts {
+	
+	public static void main (String args[]) {
+	
+		int n=5;
+		String str="Hello how r u ?";
+		char ch='r';
+		double dec=5.990;
+		boolean card= true;	
+		System.out.println("Number: "+n+"\n"+str+"\n"+ch+"\n"+dec+"\n"+card);
+		
+		//Arrays - collection of multiple values
+		int[] arr= new int[10];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=4;
+		arr[3]=12;
+		arr[4]=12;
+		arr[5]=13;
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]+1);
+		}
+		String[] stris= {"Sd","sadas","sdfs","asadfsa"};
+		for(int i=0;i<stris.length;i++)
+		{
+			System.out.println(stris[i]);
+		}
+		for (String s: stris) 
+		{
+			System.out.println(s);
+		}
+		
+		//in the case of array we need to use length and in arraylist we need to use size
+		ArrayList<String> a=new ArrayList<String>();
+		a.add("tsthe");
+		a.add("teshd");
+		a.add("tegh");
+		a.add("tesdvh");
+		System.out.println("********************************************************************************");
+		for (int i=0;i<a.size();i++)
+		{
+			System.out.println(a.get(i));
+		}
+ 	}	
+}
---
+++ src/MyFirstProg.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class MyFirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Invoking browsers
+		//ChromeDriver is class ->method ->A WebDriver implementation that controls a Chrome browser running on the local machine.It 			requires a chromedriver executable to be available in PATH.
+		//method name will be same
+		//chromedriver.exe -> to invoke the chrome browser
+		//step to invoke chrome Browsers
+		//selenium Manager - Selenium Manager is a command-line tool implemented in Rust that provides automated driver and browser 		//management for Selenium. Selenium bindings use this tool by default, so you do not need to download it or add anything to 		your code or do anything else to use it
+		System.setProperty("webdriver.chrome.driver","D:\\SHWETA\\SELENIUM\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();// to access the chrome browser 
+		driver.get("https://github.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource());
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getWindowHandles());
+		driver.close();//Close the current window, quitting the browser if it's the last window currently open.
+		driver.quit();//Quits this driver, closing every associated window.
+	}
+
+}
